35a9b2941b469a99fe06a89a71a3c996db8b2aa8
MrBr1ghtsid3 <tsvetoslav.shalev@gmail.com>
2021-03-19 17:52:09 +0000

    Update index and css

--- js/functionality.js
+++ js/functionality.js
@@ -1,3 +1,13 @@
+// Dynamic date
 let year = new Date().getFullYear();
 
 document.getElementById('year').innerHTML = year;
+
+// Burger menu
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.nav')
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+})

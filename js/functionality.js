1795557b50fd35246326fd16b5c868468401585c
mrbr1ghtsid3 <tsvetoslav.shalev@gmail.com>
2021-04-13 15:48:52 +0100

    Update CSS and HTML for about and contact

--- js/functionality.js
+++ js/functionality.js
@@ -1,9 +1,11 @@
 // Dynamic date
+
 let year = new Date().getFullYear();
 
 document.getElementById('year').innerHTML = year;
 
 // Burger menu
+
 const toggle = document.querySelector('.toggle')
 const navigation = document.querySelector('.nav')
 
@@ -11,3 +13,17 @@ toggle.addEventListener('click', () => {
   toggle.classList.toggle('active')
   navigation.classList.toggle('active')
 })
+
+// Hides footer based on user scrolling up/down the page
+
+let prevScrollPosTop = window.pageYOffset;
+
+window.onscroll = function() {
+let currentScrollPosTop = window.pageYOffset;
+  if (prevScrollPosTop > currentScrollPosTop) {
+    document.getElementById("footer").style.bottom = "-1100px";
+  } else {
+    document.getElementById("footer").style.bottom = "0px";
+  }
+  prevScrollPosTop = currentScrollPosTop;
+};
